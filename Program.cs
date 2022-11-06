c266aa66850d2e55a21c0d4410c205b70d2e784d
Marcos Leite <30819867+mabner@users.noreply.github.com>
2022-11-05 22:55:33 -0300

    testando as classes

--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
-﻿using DesafioPOO.Models;
+﻿using System.Globalization;
+using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Smartphone nokia = new Nokia(numero: "123456", modelo: "N95", imei: "1234567891234", memoria: 128);
+nokia.Ligar();
+nokia.MostrarModelo("Nokia", nokia.Modelo, nokia.Memoria);
+nokia.InstalarAplicativo("Fishing Planet");
+nokia.InstalarAplicativo("Spotify");
+
+Console.WriteLine("\n");
+
+Smartphone iphone = new Iphone("45678", "6S", "123789852741", 256);
+iphone.Ligar();
+iphone.InstalarAplicativo("Magic Hour");
+iphone.ReceberLigacao();
